3d8b259d349ece247df6b9738d53b2c47be27c37
Serhii Rusin <psychoteras@mail.ru>
2018-09-13 18:26:01 +0300

    IC

---
+++ IPCLogger.ConfigurationService/Entities/PageModel.cs
@@ -0,0 +1,32 @@
+﻿namespace IPCLogger.ConfigurationService.Entities
+{
+    public enum PageType
+    {
+        Loggers,
+        Users
+    }
+
+    public class PageModel
+    {
+        public PageType PageType;
+        public string Caption;
+        public object Model;
+
+        public PageModel(PageType pageType, string caption, object model)
+        {
+            PageType = pageType;
+            Caption = caption ?? pageType.ToString();
+            Model = model;
+        }
+
+        public static PageModel Loggers(object model)
+        {
+            return new PageModel(PageType.Loggers, null, model);
+        }
+
+        public static PageModel Users(object model)
+        {
+            return new PageModel(PageType.Users, null, model);
+        }
+    }
+}
--- IPCLogger.ConfigurationService/Web/modules/ModuleHome.cs
+++ IPCLogger.ConfigurationService/Web/modules/ModuleHome.cs
@@ -1,5 +1,9 @@
-﻿using Nancy;
+﻿using IPCLogger.ConfigurationService.DAL;
+using IPCLogger.ConfigurationService.Entities;
+using IPCLogger.ConfigurationService.Entities.Models;
+using Nancy;
 using Nancy.Security;
+using System.Collections.Generic;
 
 namespace IPCLogger.ConfigurationService.Web.modules
 {
@@ -10,7 +14,23 @@ namespace IPCLogger.ConfigurationService.Web.modules
             Get["/"] = x =>
             {
                 //this.RequiresAuthentication();
-                return View["index"];
+
+                List<LoggerModel> loggers = LoggerDAL.Instance.GetLoggers(false);
+                return View["index", PageModel.Loggers(loggers)];
+            };
+            Get["/loggers"] = x =>
+            {
+                //this.RequiresAuthentication();
+
+                List<LoggerModel> loggers = LoggerDAL.Instance.GetLoggers(false);
+                return View["index", PageModel.Loggers(loggers)];
+            };
+            Get["/users"] = x =>
+            {
+                //this.RequiresAuthentication();
+
+                List<UserModel> users = UserDAL.Instance.GetUsers();
+                return View["index", PageModel.Users(users)];
             };
         }
     }
