b8235ab5be90bbd0059d35d6eae5144dd48931fc
PsychoTeras <psychoteras@mail.ru>
2018-12-05 00:57:38 +0200

    IC

---
+++ IPCLogger.Core/Attributes/ConsoleColorConversionAttribute.cs
@@ -0,0 +1,12 @@
+﻿using IPCLogger.Core.Common;
+
+namespace IPCLogger.Core.Attributes
+{
+    public sealed class ConsoleColorConversionAttribute : CustomConversionAttribute
+    {
+        public ConsoleColorConversionAttribute()
+            : base(ConversionSource.XmlNode)
+        {
+        }
+    }
+}
