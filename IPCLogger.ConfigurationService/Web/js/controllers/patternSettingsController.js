7b1cd39555b040f08da106517ff989142c3794b9
PsychoTeras <psychoteras@gmail.com>
2019-02-10 01:29:41 +0200

    IC

---
+++ IPCLogger.ConfigurationService/Web/js/controllers/patternSettingsController.js
@@ -0,0 +1,26 @@
+﻿function PatternSettingsController() {
+
+    function createOrUpdate(applicationId, patternId, settings, propertyObjs, callback) {
+        var url = getApiUrl("applications", applicationId, "patterns", patternId, settings);
+        asyncQuery(
+            url,
+            "POST",
+            "json",
+            function (data) {
+                var failedProps = data.length ? data : null;
+                callback(failedProps, propertyObjs);
+            },
+            null,
+            JSON.stringify(propertyObjs));
+    }
+
+    this.create = function (applicationId, patternId, propertyObjs, callback) {
+        createOrUpdate(applicationId, patternId, undefined, propertyObjs, callback);
+    };
+
+    this.update = function (applicationId, patternId, propertyObjs, callback) {
+        createOrUpdate(applicationId, patternId, "settings", propertyObjs, callback);
+    };
+}
+
+window.PatternSettingsController = new PatternSettingsController();
---
+++ IPCLogger.ConfigurationService/Web/js/views/patternSettings.js
@@ -0,0 +1,133 @@
+﻿(function () {
+
+    var dictControls;
+
+    function getApplicationId() {
+        return $("#pattern-settings #application-id").val();
+    }
+
+    function getPatternId() {
+        return $("#pattern-settings #pattern-id").val();
+    }
+
+    function isNew() {
+        return $("#pattern-settings #is-new").val() === "True";
+    }
+
+    function hasChanges() {
+        var result = false;
+        $.each(dictControls,
+            function(_, item) {
+                result = isNew() || item.control.isChanged();
+                return !result;
+            });
+        return result;
+    }
+
+    function getPropertyObjs() {
+        var propertyObjs = [];
+        $.each(dictControls,
+            function (_, item) {
+
+                var control = item.control;
+                if (control.isChanged() || control.isRequired() || isNew()) {
+                    var propertyObj = control.getPropertyObject();
+                    if (!propertyObj) {
+                        throw "PropertyObject is undefined, control " + control.name();
+                    }
+                    propertyObj.isChanged |= isNew();
+                    propertyObjs.push(propertyObj);
+                }
+            });
+        return propertyObjs;
+    }
+
+    function highlightValidationErroredControls(failedProps) {
+        $.each(failedProps,
+            function(_, p) {
+                var pName = p.isCommon ? "#" + p.name : p.name;
+                var control = $.grep(dictControls, function(c) { return c.name === pName; })[0].control;
+                control.setValidity(false, p.errorMessage);
+            });
+    }
+
+    function processSaveResult(failedProps) {
+        if (failedProps) {
+            highlightValidationErroredControls(failedProps);
+        } else {
+            $.each(dictControls,
+                function (_, item) {
+                    item.control.saveOrigValue();
+                });
+            if (!cancel(true)) {
+                $.alert("Changes successfully saved", { type: "success" });
+            }
+        }
+    }
+
+    function savePropertyObjs(propertyObjs) {
+        var applicationId = getApplicationId();
+        var patternId = getPatternId();
+        if (isNew()) {
+            PatternSettingsController.create(applicationId, patternId, propertyObjs, processSaveResult);
+        } else {
+            PatternSettingsController.update(applicationId, patternId, propertyObjs, processSaveResult);
+        }
+    }
+
+    function prepareForSave() {
+        $.each(dictControls,
+            function (_, item) {
+                item.control.setValidity(true);
+            });
+    }
+
+    function save() {
+        prepareForSave();
+        var propertyObjs = getPropertyObjs();
+        if (propertyObjs.length) {
+            savePropertyObjs(propertyObjs);
+        } else {
+            cancel(true);
+        }
+    }
+
+    function cancel(force) {
+        if (force) {
+            window.onbeforeunload = undefined;
+        }
+
+        var applicationId = getApplicationId();
+        ApplicationsController.applicationSettings(applicationId, "patterns");
+        return true;
+    }
+
+    function reset() {
+        $.each(dictControls,
+            function (_, item) {
+                item.control.resetValue();
+                item.control.setValidity(true);
+            });
+    }
+
+    function initialize() {
+        initToolBar();
+
+        dictControls = new UI.ControlsFactory("#pattern-settings div.form-control");
+        $.each(dictControls,
+            function (_, item) {
+                var $control = $(item.control);
+                $control.on("showFormattable", showFormattablePopup);
+            });
+
+        $("#pattern-settings .div-save-cancel #btn-save").on("click", save);
+        $("#pattern-settings .div-save-cancel #btn-cancel").on("click", cancel);
+        $("#pattern-settings .div-save-cancel #btn-reset").on("click", reset);
+
+        window.onbeforeunload = function () {
+            return hasChanges() ? "Your changes will be lost. Continue?" : undefined;
+        };
+    }
+
+    initialize();
+})();
